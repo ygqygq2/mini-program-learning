f73926d0f7e036a6ba539ddb8f3fce922e893b81
ygqygq2 <ygqygq2@qq.com>
2022-12-18 22:39:47 +0800

    feat: 进度条

--- miniprogram/pages/2.1/index.js
+++ miniprogram/pages/2.1/index.js
@@ -1,38 +1,66 @@
+// pages/2.1/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    iconSize: [20, 30, 40, 50, 60, 100],
-    iconColor: [
-      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
-    ],
-    iconType: [
-      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
-    ],
-    iconName:'icon-sun',
-    percentValue: 80
+
   },
-  onProgressActiveEnd(e){
-    console.log(e)
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
   },
-onTapProgressBar(e){
-  console.log(e)
-  let progress = this.data.percentValue
-  if (progress < 100){
-    progress += 5
-    this.setData({percentValue:Math.min(100, progress)})
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
   }
-},
-onTapReloadBtn(e){
-  this.setData({percentValue:0})
-  this.setData({percentValue:50})
-},
-drawProgress(){
-  if (this.data.percentValue >= 100){
-    this.setData({
-      percentValue:0
-    })
-  }
-  this.setData({
-    percentValue:this.data.percentValue+10
-  })
-}
 })
